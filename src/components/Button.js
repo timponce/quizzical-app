9f8f86fdd4ad04b69603905a771f7b8b877ddb42
Timothy Ponce <timothyjponce@gmail.com>
2022-01-28 13:00:59 -0800

    Add game with trivia question from API and function to score answers

---
+++ src/components/Button.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Button(props) {
+  const initialStyles = {
+    backgroundColor: props.isSelected ? "#D6DBF5" : "#F5F7Fb",
+    border: props.isSelected ? "1px solid #D6DBF5" : "1px solid #4D5B9E",
+  };
+  const answerStyles = {
+    opacity: props.isCorrect ? "1" : "0.5",
+    backgroundColor: props.isCorrect
+      ? "#94D7A2"
+      : props.isSelected && !props.isCorrect
+      ? "#F8BCBC"
+      : "#F5F7Fb",
+    border: props.isCorrect
+      ? "1px solid #94D7A2"
+      : props.isSelected && !props.isCorrect
+      ? "1px solid #F8BCBC"
+      : "1px solid #4D5B9E",
+  };
+  console.log(props.isCorrect);
+  return (
+    <button
+      key={props.j}
+      className="answer-button"
+      style={props.checkAnswers ? answerStyles : initialStyles}
+      onClick={() => props.selectAnswer(props.option, props.questionIndex)}
+    >
+      {props.option}
+    </button>
+  );
+}
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,9 +1,29 @@
-import React from "react"
+import React from "react";
+import Button from "./Button";
 
-export default function Questions() {
+export default function Questions(props) {
+  const questionElements = props.triviaData.map((item, i) => {
+    const allAnswers = [...item.incorrect_answers, item.correct_answer];
     return (
-        <div>
-            <h2>Question</h2>
+      <div key={i} className="trivia-card">
+        <h2 className="question">{item.question}</h2>
+        <div className="answer-buttons">
+          {allAnswers.map((option, j) => (
+            <Button
+              option={option}
+              selectAnswer={props.selectAnswer}
+              questionIndex={i}
+              key={j}
+              isSelected={item.selected_answer === option}
+              isCorrect={item.correct_answer === option}
+              checkAnswers={props.checkAnswers}
+            />
+          ))}
         </div>
-    )
+        <hr></hr>
+      </div>
+    );
+  });
+
+  return <div>{questionElements}</div>;
 }
